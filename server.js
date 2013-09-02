eab08e861db805f848e56cbfea1ca473dd1a451b
Jeremy Sellars <jeremy.sellars@softekinc.com>
2013-09-02 10:23:48 -0500

    fixed startup report of port

--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 require('coffee-script');
 var app = require('./app');
 
+var port = parseInt(process.env.port || 80);
 var http = require('http');
 var httpServer = http.createServer(app);
-httpServer.listen(parseInt(process.env.port || 80));
+httpServer.listen(port);
 
-console.log("Express server listening on port %d in %s mode for %s", httpServer.address.port, app.settings.env, app.settings.recipeRoot);
+console.log("Express server listening on port %d in %s mode for %s", port, app.settings.env, app.settings.recipeRoot);
 
