06618910fdb0a0e52c158fc23e087a66ee7a56e5
Jeremy Sellars <jeremyrsellars@gmail.com>
2013-12-14 11:05:59 -0600

    removed debugging text

--- server.js
+++ server.js
@@ -7,7 +7,5 @@ var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(port);
 
-for (var v in process.env) if (v[0] != 'n') console.log(v + '=' + process.env[v]);
-
 console.log("Express server listening on port %d in %s mode for %s", port, app.settings.env, app.settings.recipeRoot);
 
